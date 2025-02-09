f729fd46dd4a1d624db71420d0fcad9c90e84f20
JosheevRai <josheev@gmail.com>
2025-02-08 22:44:34 -0500

    conduct regression on PPP adjusted gdp per capita

--- convergence-r/base-regress.R
+++ convergence-r/base-regress.R
@@ -1,7 +1,14 @@
 library(dplyr)
 library(tidyr)
 
+# Read GDP per capita data
 gdp_pc_usd <- read.csv("data/gdp-pc-usd.csv")
+gdp_pc_usd <- read.csv("data/gdp-pc-ppp.csv")
 
-regression <- lm((lag_gdp - gdp)~ gdp, data = gdp_pc_usd)
-summary(regression)
+# Regress on data
+regression_usd <- lm((lag_gdp - gdp)~ gdp, data = gdp_pc_usd)
+regression_ppp <- lm((lag_gdp - gdp)~ gdp, data = gdp_pc_ppp)
+
+# Print summary statistics of regression
+summary(regression_usd)
+summary(regression_ppp)
--- convergence-r/data.R
+++ convergence-r/data.R
@@ -2,18 +2,24 @@
 library(dplyr)
 library(tidyr)
 
-# Read GDP per capita by USD data
+# Read GDP per capita csv files
 gdp_usd_data <- read.csv("data/gdp-usd.csv")
+gdp_ppp_data <- read.csv("data/gdp-ppp.csv")
 
 # Remove unneccessary columns and years
 gdp_usd_data <- subset(gdp_usd_data, select = -c(Country.Code, Indicator.Name, Indicator.Code))
 gdp_usd_data <- gdp_usd_data[, !(names(gdp_usd_data) %in% c(paste0("X", 1960:2013), paste0("X", 2020:2024)))]
 
+gdp_ppp_data <- subset(gdp_ppp_data, select = -c(Country.Code, Indicator.Name, Indicator.Code))
+gdp_ppp_data <- gdp_ppp_data[, !(names(gdp_ppp_data) %in% c(paste0("X", 1960:2013), paste0("X", 2020:2024)))]
+
 # Omit rows with na values
 gdp_usd_data <- na.omit(gdp_usd_data)
+gdp_ppp_data <- na.omit(gdp_ppp_data)
 
-# Log-transform gdp values
+# Log-transform GDP per capita values
 gdp_usd_data[,-1] <- log(gdp_usd_data[,-1])
+gdp_ppp_data[,-1] <- log(gdp_ppp_data[,-1])
 
 # Pivot to long format and clean year values
 gdp_pc_usd <- gdp_usd_data %>%
@@ -27,6 +33,18 @@ gdp_pc_usd <- gdp_usd_data %>%
   mutate(year = as.numeric(year)) %>%
   arrange(Country.Name, year)
 
+gdp_pc_ppp <- gdp_ppp_data %>%
+  pivot_longer(
+    cols = -Country.Name,
+    names_to = "year",
+    names_prefix = "X",
+    values_to = "gdp"
+  ) %>%
+  filter(grepl("^\\d+$", year)) %>%
+  mutate(year = as.numeric(year)) %>%
+  arrange(Country.Name, year)
+
+# Create leading (next time step) column
 gdp_pc_usd <- gdp_pc_usd %>%
   group_by(Country.Name) %>%
   mutate(
@@ -34,4 +52,13 @@ gdp_pc_usd <- gdp_pc_usd %>%
   ) %>%
   ungroup()
 
+gdp_pc_ppp <- gdp_pc_ppp %>%
+  group_by(Country.Name) %>%
+  mutate(
+    lag_gdp = lead(gdp),
+  ) %>%
+  ungroup()
+
+# Save as csv
 write.csv(gdp_pc_usd, "data/gdp-pc-usd.csv", row.names = FALSE)
+write.csv(gdp_pc_ppp, "data/gdp-pc-ppp.csv", row.names = FALSE)
